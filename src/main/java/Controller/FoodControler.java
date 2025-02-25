fdd62ff61df0934b9d8b86335aafe7237c63e2f9
dragenlord <K.Malkin2009@yandex.ru>
2025-02-25 16:03:19 +0300

    /add id food and food title

--- src/main/java/Controller/FoodControler.java
+++ src/main/java/Controller/FoodControler.java
@@ -1,11 +1,13 @@
 package Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import repositoriy.Food;
 
 import java.util.List;
-
+@RestController
 public class FoodControler {
+
     @GetMapping
     public List<Food> helloWorld(){
         return List.of(
